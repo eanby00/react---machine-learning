d09924c9b9c46eac1dd07a318d81e2abbc126c5c
eanby00 <wjdwnstjd00@gmail.com>
2021-01-21 00:03:56 +0900

    0121

--- main/src/components/ModelDatas.js
+++ main/src/components/ModelDatas.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import ModelData from "./ModelData";
-// import Paper from "@material-ui/core/Paper";
+import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core";
+
+const styles = theme => ({
+  root: {
+    margin: theme.spacing(3),
+  },
+  paper: {
+    marginLeft: 18,
+    marginRight: 18
+  },
+  table: {
+    minwidth: 1080
+  },
+  tableHead: {
+    fontSize: "1.0rem"
+  }
+});
 
 class ModelDatas extends Component{
   constructor(props){
@@ -15,30 +32,35 @@ class ModelDatas extends Component{
     }
   }
     render(){
+      const { classes } = this.props;
       var lists = [];
       for (var i in this.state.datas){
-        console.log(this.state.datas[i]);
         lists.push(<ModelData key={this.state.datas[i].id} data={this.state.datas[i]}></ModelData>)
       }
+
       return(
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>이름</TableCell>
-            <TableCell>회귀/분류</TableCell>
-            <TableCell>loss 종류</TableCell>
-            <TableCell>loss</TableCell>
-            <TableCell>정확도</TableCell>
-            <TableCell>언어 종류</TableCell>
-            <TableCell>확인</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {lists}
-        </TableBody>
-      </Table>
+        <div className={classes.root}>
+          <Paper className={classes.paper}>
+            <Table className={classes.table}>
+              <TableHead className={classes.TableHead}>
+                <TableRow>
+                  <TableCell>이름</TableCell>
+                  <TableCell>회귀/분류</TableCell>
+                  <TableCell>loss 종류</TableCell>
+                  <TableCell>loss</TableCell>
+                  <TableCell>정확도</TableCell>
+                  <TableCell>언어 종류</TableCell>
+                  <TableCell>확인</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {lists}
+              </TableBody>
+            </Table>
+          </Paper>
+        </div>
       );
     }
 }
 
-export default ModelDatas;
+export default withStyles(styles) (ModelDatas);
