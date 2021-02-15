1fd43007a6f5df8851dfa2d882c4d5a8c1b9e7da
eanby00 <wjdwnstjd00@gmail.com>
2021-02-15 18:03:13 +0900

    0215

--- main/src/data_python/health_height_and_weight/height_and_weight.py
+++ main/src/data_python/health_height_and_weight/height_and_weight.py
@@ -11,38 +11,38 @@ import tensorflowjs as tfjs
 # loss: 
 # 정확도: X
 # 언어 종류: 파이썬
-# 코드 링크: 
+# 코드 링크: https://raw.githubusercontent.com/eanby00/react-machine-learning/master/main/src/data_python/health_height_and_weight/height_and_weight.py
 # 데이터 출처: https://www.data.go.kr/data/15007122/fileData.do
-# 설명: 건강검진정보 키와 몸무게
-# 데이터 링크: 
+# 설명: 건강검진정보 키와 몸무게, 데이터 고용량에 따라 파이썬만 실행
+# 데이터 링크: https://raw.githubusercontent.com/eanby00/react-machine-learning/master/main/src/data/health_height_and_weight.csv
 # 모델 링크: 
-# 독립 변수: 
-# 종속 변수: 
+# 독립 변수: height
+# 종속 변수: weight
 # 샘플 데이터 링크: 
 
 # -------------------------------------------------------------------------------
 
 ## 데이터 불러오기
-file_root = "https://raw.githubusercontent.com/eanby00/react-machine-learning/master/main/src/data/boston.csv"
-boston = pd.read_csv(file_root)
-print(boston.columns)
-print(boston.head())
+file_root = "https://raw.githubusercontent.com/eanby00/react-machine-learning/master/main/src/data/health_height_and_weight.csv"
+data = pd.read_csv(file_root)
+print(data.columns)
+print(data.head())
 
-print(len(boston))
-training_boston = boston.sample(frac=0.8)
-print(training_boston.head())
-test_boston = boston.drop(training_boston.index)
+print(len(data))
+training_data = data.sample(frac=0.8)
+print(training_data.head())
+test_data = data.drop(training_data.index)
 
 # -------------------------------------------------------------------------------
 
 # ## 저장된 모델 불러오기
-json_file = open("./main/src/data_python/boston_model/boston.json", "r")
-loaded_model_json = json_file.read()
-json_file.close()
+# json_file = open("./main/src/data_python/boston_model/boston.json", "r")
+# loaded_model_json = json_file.read()
+# json_file.close()
 
-model = model_from_json(loaded_model_json)
+# model = model_from_json(loaded_model_json)
 
-model.load_weights("./main/src/data_python/boston_model/boston.h5")
+# model.load_weights("./main/src/data_python/boston_model/boston.h5")
 
 # -------------------------------------------------------------------------------
 
@@ -51,34 +51,34 @@ model.load_weights("./main/src/data_python/boston_model/boston.h5")
 
 # -------------------------------------------------------------------------------
 
-## 종속 변수, 독립 변수로 분리
-# independent = training_boston[['crim', 'zn', 'indus', 'chas', 'nox', 'rm', 'age', 'dis', 'rad', 'tax', 'ptratio', 'b', 'lstat']]
-# dependent = training_boston[["medv"]]
+# 종속 변수, 독립 변수로 분리
+independent = training_boston[['crim', 'zn', 'indus', 'chas', 'nox', 'rm', 'age', 'dis', 'rad', 'tax', 'ptratio', 'b', 'lstat']]
+dependent = training_boston[["medv"]]
 
 # -------------------------------------------------------------------------------
 
-## 모델의 구조 제작
-# X = tf.keras.layers.Input(shape=[13])
+# 모델의 구조 제작
+X = tf.keras.layers.Input(shape=[13])
 
-# H = tf.keras.layers.Dense(13)(X)
-# H = tf.keras.layers.BatchNormalization()(H)
-# H = tf.keras.layers.Activation("swish")(H)
+H = tf.keras.layers.Dense(13)(X)
+H = tf.keras.layers.BatchNormalization()(H)
+H = tf.keras.layers.Activation("swish")(H)
 
-# H = tf.keras.layers.Dense(13)(H)
-# H = tf.keras.layers.BatchNormalization()(H)
-# H = tf.keras.layers.Activation("swish")(H)
+H = tf.keras.layers.Dense(13)(H)
+H = tf.keras.layers.BatchNormalization()(H)
+H = tf.keras.layers.Activation("swish")(H)
 
-# H = tf.keras.layers.Dense(13)(H)
-# H = tf.keras.layers.BatchNormalization()(H)
-# H = tf.keras.layers.Activation("swish")(H)
+H = tf.keras.layers.Dense(13)(H)
+H = tf.keras.layers.BatchNormalization()(H)
+H = tf.keras.layers.Activation("swish")(H)
 
-# Y = tf.keras.layers.Dense(1)(H)
-# model = tf.keras.models.Model(X,Y)
-# model.compile(loss="mse")
+Y = tf.keras.layers.Dense(1)(H)
+model = tf.keras.models.Model(X,Y)
+model.compile(loss="mse")
 
-# ## 데이터로 모델 학습
-# model.fit(independent, dependent, epochs=10000, verbose=0)
-# model.fit(independent, dependent, epochs=10)
+## 데이터로 모델 학습
+model.fit(independent, dependent, epochs=10000, verbose=0)
+model.fit(independent, dependent, epochs=10)
 
 # -------------------------------------------------------------------------------
 
@@ -87,25 +87,25 @@ model.load_weights("./main/src/data_python/boston_model/boston.h5")
 
 # -------------------------------------------------------------------------------
 
-# 모델 저장
-# model_json = model.to_json()
-# with open("main/src/data_python/boston_model/boston.json", "w") as json_file: json_file.write(model_json)
+모델 저장
+model_json = model.to_json()
+with open("main/src/data_python/boston_model/boston.json", "w") as json_file: json_file.write(model_json)
 
-# model.save_weights("main/src/data_python/boston_model/boston.h5")
+model.save_weights("main/src/data_python/boston_model/boston.h5")
 
-# TF.js Layer 형식으로 내보내기
-# tfjs.converters.save_keras_model(model, "./main/src/data_python/boston_model")
+TF.js Layer 형식으로 내보내기
+tfjs.converters.save_keras_model(model, "./main/src/data_python/boston_model")
 
 # -------------------------------------------------------------------------------
 
-## 모델을 이용
+# ## 모델을 이용
 
-temp = np.ravel(model.predict(test_boston[['crim', 'zn', 'indus', 'chas', 'nox', 'rm', 'age', 'dis', 'rad', 'tax', 'ptratio', 'b', 'lstat']]), order="C")
-result_data_predict = pd.Series(temp)
+# temp = np.ravel(model.predict(test_boston[['crim', 'zn', 'indus', 'chas', 'nox', 'rm', 'age', 'dis', 'rad', 'tax', 'ptratio', 'b', 'lstat']]), order="C")
+# result_data_predict = pd.Series(temp)
 
-result_data = pd.DataFrame(test_boston[["medv"]].index, columns=["id"])
-result_data["예측한 값"] = result_data_predict
-temp = test_boston[["medv"]].reset_index()["medv"]
-result_data["실제 값"] = temp
+# result_data = pd.DataFrame(test_boston[["medv"]].index, columns=["id"])
+# result_data["예측한 값"] = result_data_predict
+# temp = test_boston[["medv"]].reset_index()["medv"]
+# result_data["실제 값"] = temp
 
-print(result_data)
+# print(result_data)
